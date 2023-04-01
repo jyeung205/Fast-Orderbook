7473c8d2229a9ed02869a4c4cba94e440daf6dc6
Jimmy Yeung <53492347+jyeung205@users.noreply.github.com>
2023-04-01 13:30:39 +0100

    starting test_orderbook.py

--- test_orderbook.py
+++ test_orderbook.py
@@ -1,9 +1,60 @@
 import unittest
+from orderbook import OrderBook, Order
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestOrderBook(unittest.TestCase):
+    def test_add_order(self):
+        """
+        - check if order exists in order_id map
+        - check if order exists in OrderList in corresponding price value
+        - check if best bid/ask has been updated
+        """
+
+        # Test adding a new order
+        order_book = OrderBook()
+        order = {
+            'type': 'limit',
+            "price": 100,
+            'qty': 10,
+            'bid_ask': 'bid',
+            'order_id': 1
+        },
+        order_book.parse_input(order)
+
+        self.assertEqual(order_id in order_book.order_map and order_book.order_map[order_id] == Order(order))
+        # self.assertEqual(len(order_book.sell_orders), 0)
+
+    def test_cancel_order(self):
+        """
+        - check if order doesn't exist in order_id map
+        - check if order doesn't exist in OrderList in corresponding price value
+        - check best bid/ask has been updated
+        """
+        # Test canceling an existing order
+        order_book = OrderBook()
+        orders = [
+            {
+                'type': 'limit',
+                "price": 100,
+                'qty': 10,
+                'bid_ask': 'bid',
+                'order_id': 1
+            },
+            {
+                "type": 'cancel',
+                'order_id': 1,
+            },
+        ]
+        for order in orders:
+            order_book.parse_input(order)
+        # self.assertEqual(len(order_book.buy_orders), 0)
+
+    def test_execute_market_trade(self):
+        """
+        - check if best bid/ask order has been removed
+        - check if trade exists in the trade tracker
+        """
+        pass
 
 
 if __name__ == '__main__':
